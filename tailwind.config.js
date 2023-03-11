68bf87420b7c811aa126c976f130a5facabe8dcf
“steven1303” <steven.grafensen@gmail.com>
2023-03-11 21:59:18 +0700

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./resource/**/*.{blade.php,vue,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
